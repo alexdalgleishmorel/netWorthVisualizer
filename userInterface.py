0aadd032673c3d5d81d9ca36fd12583ddb197d97
alexdalgleishmorel <60409509+alexdalgleishmorel@users.noreply.github.com>
2021-09-26 20:09:06 -0600

    Removed commented out code

--- userInterface.py
+++ userInterface.py
@@ -113,16 +113,6 @@ def createMenu(initialize):
     frame.pack()
     menuGui.mainloop()
 
-"""
-
-def iconClick(event):
-    print(event)
-    for assetIcon in globals.assetIcons:
-        if assetIcon.clicked(event.x, event.y):
-            assetIcon.asset.showHistory()
-
-"""
-
 def round_rectangle(x1, y1, x2, y2, r=25, **kwargs):    
     points = (x1+r, y1, x1+r, y1, x2-r, y1, x2-r, y1, x2, y1, x2, y1+r, x2, y1+r, x2, y2-r, x2, y2-r, x2, y2, x2-r, y2, x2-r, y2, x1+r, y2, x1+r, y2, x1, y2, x1, y2-r, x1, y2-r, x1, y1+r, x1, y1+r, x1, y1)
     global menuCanvas
