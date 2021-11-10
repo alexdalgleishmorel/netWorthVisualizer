d0b9520759638621c19d2ebfab7a84ce5cd9300f
alexdalgleishmorel <60409509+alexdalgleishmorel@users.noreply.github.com>
2021-11-10 13:45:30 -0700

    Improved graphs and speculation

--- speculation.py
+++ speculation.py
@@ -1,29 +1,6 @@
 import globals as globals
 import pathlib
-
-"""
-def prospectAverageCost (asset, targetAverageCost, targetStockPrice):
-
-    priceRange = 2
-
-    startPrice = asset.currentPrice-(priceRange/2)
-
-    print("current average cost: {0}".format(asset.averageCost))
-    print("current book value: {0}".format(asset.bookValue))
-    print("current shares: {0}".format(asset.shares))
-
-    while (startPrice <= asset.currentPrice):
-
-        sharesToPurchase = ((startPrice*asset.shares)-asset.bookValue)/(asset.currentPrice-startPrice)
-        resultingCost = sharesToPurchase*asset.currentPrice
-
-        print("Target average cost --> {0}".format(startPrice))
-        print("Shares to purchase: {0:.2f} with resulting cost of: {1:.2f}".format(sharesToPurchase, resultingCost))
-        print()
-
-        startPrice += 0.1
-
-"""
+import yfinance
 
 def calculateAverageCost(asset, cash):
 
@@ -31,3 +8,13 @@ def calculateAverageCost(asset, cash):
 
     # Returning the new average cost if the user put in the given amount of cash
     return ((asset.averageCost*asset.shares)+(asset.currentPrice*sharesToBuy))/(asset.shares+sharesToBuy)
+
+def convertEthereum(asset):
+    ticker = yfinance.Ticker("ETH-USD")
+    data = ticker.history()
+    return (data.tail(1)['Close'].iloc[0])/asset.currentPrice
+
+def convertBitcoin(asset):
+    ticker = yfinance.Ticker("BTC-USD")
+    data = ticker.history()
+    return (data.tail(1)['Close'].iloc[0])/asset.currentPrice
